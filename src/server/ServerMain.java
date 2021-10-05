8a4bb3c7b2326593bc532ff019fd5e7274a6536b
longptit <vuduylong112@gmail.com>
2021-10-05 14:55:01 +0700

    Initial commit

---
+++ src/server/ServerMain.java
@@ -0,0 +1,17 @@
+package server;
+
+import javax.swing.*;
+
+public class ServerMain {
+    public static void main(String[] args) {
+
+        ServerApp server = new ServerApp();
+        server.setSize(400,250);
+        server.setVisible(true);
+        server.setTitle("Checkers Server");
+        server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //start Connection
+        server.startRunning();
+    }
+}
